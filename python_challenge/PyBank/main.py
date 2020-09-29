04cf42540f47e91207dd6a9a8030fe5f77d1151f
David Coy <42867669+coymatic@users.noreply.github.com>
2020-09-29 12:15:43 -0400

    added lines for ouput text in PyBank

--- python_challenge/PyBank/main.py
+++ python_challenge/PyBank/main.py
@@ -10,67 +10,78 @@ import csv
 #inpFile = pd.read_csv(url, sep='\t', error_bad_lines= False,quoting=csv.QUOTE_NONE)
 #pdb.set_trace()
 
-# Path to collect data from the Resources folder
-pybank_csv = os.path.join('.', 'resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
+with open('file.txt', 'w') as f:
+    # Path to collect data from the Resources folder
+    pybank_csv = os.path.join('.', 'resources', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
 
-#The total number of months included in the dataset
-count_months = 0
-with open(pybank_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    next(csv_reader)# Skips the header row
-    for row in csv_reader:
-        count_months = count_months + 1
-print(f'Total Months: {count_months}')
+    #The total number of months included in the dataset
+    count_months = 0
+    with open(pybank_csv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        next(csv_reader)# Skips the header row
+        for row in csv_reader:
+            count_months = count_months + 1
+            message = f'Total Months: {count_months}'
+    print(message, file=f)
+    print(message)
 
-#The net total amount of "Profit/Losses" over the entire period
-net_profit = 0
-with open(pybank_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    next(csv_reader)# Skips the header row
-    for row in csv_reader:
-        net_profit = net_profit + float(row[1])
-print(f'Total: ${net_profit:.0f}')
+    #The net total amount of "Profit/Losses" over the entire period
+    net_profit = 0
+    with open(pybank_csv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        next(csv_reader)# Skips the header row
+        for row in csv_reader:
+            net_profit = net_profit + float(row[1])
+    message = f'Total: ${net_profit:.0f}'
+    print(message, file=f)
+    print(message)
 
-#The average of the changes in "Profit/Losses" over the entire period
-with open(pybank_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    next(csv_reader)# Skips the header row
-    old = 0
-    change_profit=[]
-    for row in csv_reader:
-        change_profit.append(float(row[1]) - old)
-        old = float(row[1])
-#pdb.set_trace()
-print(f'Total: ${sum(change_profit[1:])/(len(change_profit)-1):.2f}')
+    #The average of the changes in "Profit/Losses" over the entire period
+    with open(pybank_csv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        next(csv_reader)# Skips the header row
+        old = 0
+        change_profit=[]
+        for row in csv_reader:
+            change_profit.append(float(row[1]) - old)
+            old = float(row[1])
+    message = f'Total: ${sum(change_profit[1:])/(len(change_profit)-1):.2f}' 
+    print(message, file=f)
+    print(message)
+    
+    #The greatest increase in profits (date and amount) over the entire period
+    with open(pybank_csv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        next(csv_reader)# Skips the header row
+        old = 0
+        change_profit=[]
+        max_change = 0
+        date = ''
+        for row in csv_reader:
+            change_profit = float(row[1]) - old
+            old = float(row[1])
+            if change_profit > max_change:
+                max_change = change_profit
+                date = row[0]
+    message = f'Greatest Increase in Profits: {date} $({max_change:.0f})' 
+    print(message, file=f)
+    print(message)
 
-#The greatest increase in profits (date and amount) over the entire period
-with open(pybank_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    next(csv_reader)# Skips the header row
-    old = 0
-    change_profit=[]
-    max_change = 0
-    date = ''
-    for row in csv_reader:
-        change_profit = float(row[1]) - old
-        old = float(row[1])
-        if change_profit > max_change:
-            max_change = change_profit
-            date = row[0]
-print(f'Greatest Increase in Profits: {date} $({max_change:.0f})')
-
-#The greatest decrease in losses (date and amount) over the entire period
-with open(pybank_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-    next(csv_reader)# Skips the header row
-    old = 0
-    change_profit=[]
-    max_dec = 0
-    date = ''
-    for row in csv_reader:
-        change_profit = float(row[1]) - old
-        old = float(row[1])
-        if change_profit < max_dec:
-            max_dec = change_profit
-            date = row[0]
-print(f'Greatest Decrease in Profits: {date} $({max_dec:.0f})')
+    #The greatest decrease in losses (date and amount) over the entire period
+    with open(pybank_csv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        next(csv_reader)# Skips the header row
+        old = 0
+        change_profit=[]
+        max_dec = 0
+        date = ''
+        for row in csv_reader:
+            change_profit = float(row[1]) - old
+            old = float(row[1])
+            if change_profit < max_dec:
+                max_dec = change_profit
+                date = row[0]
+    message = f'Greatest Decrease in Profits: {date} $({max_dec:.0f})' 
+    print(message, file=f)
+    print(message)
+    f.close()
